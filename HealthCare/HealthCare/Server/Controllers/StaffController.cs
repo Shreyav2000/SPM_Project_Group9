cc6cf090adcf1eb7f798f29e6b8c1366bbcabd4f
Sri Naga Sai Subhank Vempati <vemps01@pfw.edu>
2023-03-13 17:39:55 -0400

    Designed endpoint to look staff up by their id

---
+++ HealthCare/HealthCare/Server/Controllers/StaffController.cs
@@ -0,0 +1,40 @@
+﻿using HealthCare.Shared.Interfaces;
+using HealthCare.Shared.Objects;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
+
+namespace HealthCare.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StaffController : ControllerBase
+    {
+
+        private readonly Methods.Validator m_validator;
+        private readonly IStaffService m_service;
+
+        public StaffController(IStaffService a_staffService, ITokenService a_tokenService, IPermissionService a_permission)
+        {
+            m_service = a_staffService;
+            m_validator = new Methods.Validator(a_tokenService, a_permission);
+        }
+        /// <summary>
+        /// Endpoint to look up a staff profile using their id
+        /// </summary>
+        /// <param name="a_drug"></param>
+        [Authorize]
+        [HttpGet, Route("records/profile/{a_id}")]
+        public ActionResult<List<MedHistory>> GetPatient(int a_id)
+        {
+            string token = Request.Headers[HeaderNames.Authorization]!;
+            string? validationResult = m_validator.Validate(token, 7);
+            if (!string.IsNullOrEmpty(validationResult))
+                return BadRequest(validationResult);
+
+            var history = m_service.GetStaff(a_id);
+            return Ok(history);
+        }
+    }
+}
---
+++ HealthCare/HealthCare/Server/Methods/StaffService.cs
@@ -0,0 +1,31 @@
+﻿using HealthCare.Shared.Interfaces;
+using HealthCare.Shared.Models;
+
+namespace HealthCare.Server.Methods
+{
+    public class StaffService : IStaffService
+    {
+        private readonly HealthcareContext m_context;
+        private readonly ILogger<Staff> m_logger;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Staff"/> class.
+        /// </summary>
+        /// <param name="context">The database context.</param>
+        /// <param name="logger">The logger.</param>
+        public StaffService(HealthcareContext context, ILogger<Staff> logger)
+        {
+            m_context = context;
+            m_logger = logger;
+        }
+        /// <summary>
+        /// Looks up a staff by their staff id
+        /// </summary>
+        /// <param name="a_staffid"></param>
+        /// <returns>Staff profile is found</returns>
+        public Staff GetStaff(int a_staffid)
+        {
+            return m_context.Staff.FirstOrDefault(i => i.Staffid == a_staffid);
+        }
+    }
+}
