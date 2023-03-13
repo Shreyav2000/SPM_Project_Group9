3c9f38b0b93a7f1f8091c0f0c73213ef2d3f6459
Shreya Vithala <v01@pfw.edu>
2023-03-13 15:37:25 -0400

    deleted client bin & obj folders

--- HealthCare/HealthCare/Client/obj/Debug/net7.0/service-worker/service-worker.js
+++
@@ -1,9 +0,0 @@
-// In development, always fetch from the network and do not enable offline support.
-// This is because caching would make development more difficult (changes would not
-// be reflected on the first load after each change).
-self.addEventListener('fetch', () => { });
-<<<<<<< HEAD
-/* Manifest version: A8mL3ENw */
-=======
-/* Manifest version: Xf4uS9k+ */
->>>>>>> origin/main
