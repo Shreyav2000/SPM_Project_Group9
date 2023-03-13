3bcf27bd7df3a5c13db80b5e32e4373f5ad75970
barhen-pfw <arheb01@pfw.edu>
2023-03-13 19:11:06 -0400

    Analysis patch

--- HealthCare/HealthCare/Shared/Objects/DrugAnalysis.cs
+++ HealthCare/HealthCare/Shared/Objects/DrugAnalysis.cs
@@ -10,8 +10,8 @@ namespace HealthCare.Shared.Objects
     public class DrugAnalysis
     {
         public List<Expiryitem> ExpiredDrugs { get; set; }
-        public string MostPurchasedDrug { get; set; }
-        public string MostPrescribedDrug { get; set; }
+        public string? MostPurchasedDrug { get; set; }
+        public string? MostPrescribedDrug { get; set; }
         public List<Drug> ReplenishedDrugs { get; set; }
         public string? MostExpensiveDrug { get; set; }
     }
