66335b588b1004f703a79dfe929b05e6e141c7d6
Shreya Vithala <v01@pfw.edu>
2023-03-25 00:16:02 -0400

    added Interface and controller for system metrics

---
+++ HealthCare/HealthCare/Server/Controllers/SystemMetricsController.cs
@@ -0,0 +1,30 @@
+﻿using HealthCare.Shared.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+
+[ApiController]
+public class SystemMetricsController : ControllerBase
+{
+    private readonly ISystemMetricsService _systemMetricsService;
+
+    public SystemMetricsController(ISystemMetricsService systemMetricsService)
+    {
+        _systemMetricsService = systemMetricsService;
+    }
+
+    [HttpGet]
+    [Route("api/systemmetrics/cpuusage")]
+    public async Task<ActionResult<double>> GetCpuUsage()
+    {
+        var cpuUsage = await _systemMetricsService.GetCpuUsageAsync();
+        return Ok(cpuUsage);
+    }
+
+    [HttpGet]
+    [Route("api/systemmetrics/memoryusage")]
+    public async Task<ActionResult<double>> GetMemoryUsage()
+    {
+        var memoryUsage = await _systemMetricsService.GetMemoryUsageAsync();
+        return Ok(memoryUsage);
+    }
+}
---
+++ HealthCare/HealthCare/Shared/Interfaces/ISystemMetricsService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HealthCare.Shared.Interfaces
+{
+    public interface ISystemMetricsService
+    {
+        public Task<double> GetCpuUsageAsync();
+        public Task<double> GetMemoryUsageAsync();
+    }
+
+    public class SystemMetricsService : ISystemMetricsService
+    {
+        public async Task<double> GetCpuUsageAsync()
+        {
+            var process = Process.GetCurrentProcess();
+            var startTime = DateTime.UtcNow - process.TotalProcessorTime;
+            await Task.Delay(500); // Wait for half a second to get a more accurate reading
+            var endTime = DateTime.UtcNow;
+            var cpuTime = (process.TotalProcessorTime.TotalMilliseconds / (endTime - startTime).TotalMilliseconds) * 100;
+            return Math.Round(cpuTime, 2);
+        }
+
+        public Task<double> GetMemoryUsageAsync()
+        {
+            var process = Process.GetCurrentProcess();
+            var memoryUsage = process.PrivateMemorySize64 / 1024.0; // Convert bytes to megabytes
+            return Task.FromResult(Math.Round(memoryUsage, 2));
+        }
+    }
+}
